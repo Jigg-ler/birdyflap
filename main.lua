3bab0fe981802e62581b33b7cc8124b2c63fc56a
mmtapalla <gm.tapallamiguel@gmail.com>
2020-11-18 18:09:41 +0800

    Added assets (placeholders only) and parallax effect to main.lua file

--- main.lua
+++ main.lua
@@ -1,10 +1,23 @@
 push = require 'push'
 
 --window parameters
-WINDOW_HEIGHT = 1280
-WINDOW_WIDTH = 720
-VIRTUAL_HEIGHT = 512
-VIRTUAL_WIDTH = 288
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+--load assets
+local background = love.graphics.newImage('assets/background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('assets/ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     love.window.setTitle('Birdy Flap')
@@ -27,11 +40,26 @@ function love.keypressed(key)
 
 end
 
+function love.update(dt)
+    -- scroll background by preset speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
+        % BACKGROUND_LOOPING_POINT
+
+    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
+        % VIRTUAL_WIDTH
+end
 
 function love.draw()
     push:start()
+        
+  -- draw the background at the negative looping point
+  love.graphics.draw(background, -backgroundScroll, 0)
 
-    
+  -- draw the ground on top of the background, toward the bottom of the screen,
+  -- at its negative looping point
+  love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+  
     push:finish()
 end
 
