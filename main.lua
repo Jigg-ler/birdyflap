53b733123e469403c8e926e655d2325bf81b5ef8
Jigg-ler <angelolobabia@gmail.com>
2020-11-18 13:17:59 +0800

    added a bit more but still needs bg to progress further

--- main.lua
+++ main.lua
@@ -7,8 +7,17 @@ VIRTUAL_HEIGHT = 512
 VIRTUAL_WIDTH = 288
 
 function love.load()
+    love.window.setTitle('Birdy Flap')
 
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
 end
+
 
 function love.keypressed(key)
 
@@ -18,8 +27,15 @@ function love.keypressed(key)
 
 end
 
+
 function love.draw()
     push:start()
 
+    
     push:finish()
+end
+
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
