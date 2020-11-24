098fdc8190dfeafa40aa3a2921109747efb6aae9
Jigg-ler <angelolobabia@gmail.com>
2020-11-25 03:37:35 +0800

    implemented until collision and added temporary upper bounds

--- main.lua
+++ main.lua
@@ -19,7 +19,7 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
---load assets
+--load assets and their needed values
 local background = love.graphics.newImage('assets/background.png')
 local backgroundScroll = 0
 
@@ -34,11 +34,13 @@ local BACKGROUND_LOOPING_POINT = 413
 --player
 local player = Player()
 
+--pipes
 local pipePairs = {}
-
 local spawnTimer = 0
-
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+--the game is scrolling
+local scrolling = true
 
 function love.load()
     love.window.setTitle('Birdy Flap')
@@ -52,10 +54,15 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    --creates a table 'love.keyboard' i think hahaha
+    love.keyboard.keysPressed = {}
 end
 
 
 function love.keypressed(key)
+    --equates 'love.keyboard.keysPressed[key]' to true
+    love.keyboard.keysPressed[key] = true
 
     if key == 'escape' then
         love.event.quit()
@@ -63,36 +70,64 @@ function love.keypressed(key)
 
 end
 
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
 function love.update(dt)
-    -- scroll background by preset speed * dt, looping back to 0 after the looping point
-    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
-        % BACKGROUND_LOOPING_POINT
+    if scrolling then
+        -- scroll background by preset speed * dt, looping back to 0 after the looping point
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
+            % BACKGROUND_LOOPING_POINT
 
-    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
-        % VIRTUAL_WIDTH
+        -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
+            % VIRTUAL_WIDTH
 
-    spawnTimer = spawnTimer + dt
+        spawnTimer = spawnTimer + dt
 
-    if spawnTimer > 2 then
-        local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        lastY = y
+        if spawnTimer > 2 then
+            local y = math.max(-PIPE_HEIGHT + 10, math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            lastY = y
 
-        table.insert(pipePairs, PipePair(y))
-        spawnTimer = 0
-    end
-
-    for k, pair in pairs(pipePairs) do
-        pair:update(dt)
-    end
-
-    for k, pair in pairs(pipePairs) do
-        if pair.remove then
-            table.remove(pipePairs, k)
+            table.insert(pipePairs, PipePair(y))
+            spawnTimer = 0
         end
+
+        player:update(dt)
+
+        --temporary upper bounds
+        if player.y <= 0 then
+            player.y = 0
+            player.dy = 0
+        end
+
+        for k, pair in pairs(pipePairs) do
+            pair:update(dt)
+
+            --checks if player collides with any of the pipes
+            for l, pipe in pairs(pair.pipes) do
+                if player:collides(pipe) then
+                    scrolling = false
+                end
+            end
+
+        end
+
+        for k, pair in pairs(pipePairs) do
+            if pair.remove then
+                table.remove(pipePairs, k)
+            end
+        end
+        
     end
 
-
+    --before the frame ends it empties the table
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
