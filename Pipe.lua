b38d1c81d578436e688420548366a6de54dd2a7d
Jigg-ler <angelolobabia@gmail.com>
2020-11-25 03:36:01 +0800

    modified the instance of PIPE_HEIGHT and PIPE_WIDTH

--- Pipe.lua
+++ Pipe.lua
@@ -4,14 +4,14 @@ local PIPE_IMAGE = love.graphics.newImage('assets/pipe.png')
 
 PIPE_SPEED = 60
 
-PIPE_HEIGHT = 288
-PIPE_WIDTH = 70
+PIPE_HEIGHT = PIPE_IMAGE:getHeight()
+PIPE_WIDTH = PIPE_IMAGE:getWidth()
 
 function Pipe:init(orientation, y)
     self.x = VIRTUAL_WIDTH
     self.y = y
 
-    self.width = PIPE_IMAGE:getWidth()
+    self.width = PIPE_WIDTH
     self.height = PIPE_HEIGHT
 
     self.orientation = orientation 
@@ -22,5 +22,8 @@ function Pipe:update(dt)
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x, (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 0, 1, self.orientation == 'top' and -1 or 1)
+    love.graphics.draw(PIPE_IMAGE, self.x, (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 
+    0, 
+    1, 
+    self.orientation == 'top' and -1 or 1)
 end
